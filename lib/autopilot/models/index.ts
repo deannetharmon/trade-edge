0c755994edb575f896159252bfca3f116b4fc30d
Dean Harmon <dean.harmon@gmail.com>
2026-07-02 01:03:54 -0600

    feat(autopilot): add model exports

---
+++ lib/autopilot/models/index.ts
@@ -0,0 +1,20 @@
+// lib/autopilot/models/index.ts
+
+export type {
+  AutopilotCandidate,
+  AutopilotConfig,
+  AutopilotDecisionAction,
+  AutopilotDecisionLogEntry,
+  AutopilotGoal,
+  AutopilotLeg,
+  AutopilotRunResult,
+  AutopilotStrategy,
+  DecisionConfidenceBreakdown,
+  DecisionConfidenceInput,
+  OpportunityScoreBreakdown,
+  PaperAccount,
+  PaperEquityPoint,
+  PaperPosition,
+  PaperPositionStatus,
+  PortfolioRiskPosture,
+} from '../types';
