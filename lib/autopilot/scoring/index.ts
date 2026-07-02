6cf66dae2fec52be4bb1a271ec32bda179ff5921
Dean Harmon <dean.harmon@gmail.com>
2026-07-02 01:27:33 -0600

    feat(autopilot): add scoring exports

---
+++ lib/autopilot/scoring/index.ts
@@ -0,0 +1,6 @@
+// lib/autopilot/scoring/index.ts
+
+export { calculateDecisionConfidence } from './confidence';
+export { calculateNetEdge, calculateNetEdgeFadePct } from './netEdge';
+export type { NetEdgeInput, NetEdgeResult } from './netEdge';
+export { calculateOpportunityScore } from './opportunity';
