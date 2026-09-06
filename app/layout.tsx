a766c91f2048252c5a7dcd1094087278c857c056
Dean Harmon <dean.harmon@gmail.com>
2026-09-06 00:20:37 -0600

    Add antialiased class to body element

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="antialiased">
         <Providers>{children}</Providers>
       </body>
     </html>
